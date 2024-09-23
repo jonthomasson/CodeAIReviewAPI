d1e9cf04a14df8c0fce2ba0175b54c4cb20f2a00
Jon Thomasson <jthomasson@sjcoe.net>
2024-09-22 17:32:19 -0700

    adding resource file to hold ai prompts.

--- Services/AiService.cs
+++ Services/AiService.cs
@@ -1,6 +1,7 @@
 ﻿using Azure;
 using Azure.AI.OpenAI;
 using CodeAIReviewAPI.Models;
+using CodeAIReviewAPI.Resources;
 using OpenAI.Chat;
 using System.Text.Json;
 
@@ -18,6 +19,7 @@ namespace CodeAIReviewAPI.Services
             // Get environment variables for OpenAI
             string endpoint = _configuration["AzureAI:Endpoint"];
             string key = _configuration["AzureAI:Key"];
+            string system_prompt = AIPrompts.code_review_system_prompt;
 
             // Create the OpenAI client
             AzureKeyCredential credential = new AzureKeyCredential(key);
@@ -27,7 +29,7 @@ namespace CodeAIReviewAPI.Services
             ChatCompletion completion = chatClient.CompleteChat(
                 [
                     // System messages represent instructions or other guidance about how the assistant should behave
-                    new SystemChatMessage("You're an AI code reviewer. You get pull request file changes and you reply with helpful code suggestions if appropriate. If the code is C#, you should use common C# code conventions as explained here: https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/coding-conventions. If the code is Angular, use the Angular style guide. If a different framework, try to determine the framework and use the appropriate style guide and standards. You will receive the file changes in a JSON packet like this [{\"fileName\": \"GitTestMerge/Services/Service1.cs\", \"patch\": \"code updates and deletions\"}]. After processing the data, return the results as a JSON packet like this {\"standards\": \"comma separated list with one or more standards applied to the code (example Angular Style Guide, C# Style Guide etc).\", \"score\": \"score is a number from 1 to 10. 1 would be poor quality code, 10 would be exceptional code quality.\", \"codeReview\":[{\"fileName\": \"fileName\", \"review\": \"string will contain the code suggestions, if any. Can be left empty if there are no suggestions.\"}]}"),
+                    new SystemChatMessage(system_prompt),
                     new UserChatMessage(JsonSerializer.Serialize(fileChanges)),
                 ],
                  new ChatCompletionOptions()
